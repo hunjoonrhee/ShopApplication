7540282a71d1801785ff7a1e23489e2be03df396
hunjoonrhee <hunjoonrhee90@gmail.com>
2022-09-11 19:13:48 +0200

    Add Tests with assertThat

--- src/test/java/service/ShopServiceTest.java
+++ src/test/java/service/ShopServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import repo.OrderRepo;
 import repo.ProductRepo;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShopServiceTest {
@@ -26,7 +27,9 @@ class ShopServiceTest {
        // THEN
         Product expectedProduct = productRepo.get(product1.getId());
 
-        assertTrue(expectedProduct.equals(actual));
+//        assertTrue(expectedProduct.equals(actual));
+        //Using AssertJ
+        assertThat(expectedProduct).isEqualTo(actual);
     }
 
     @Test
@@ -62,11 +65,35 @@ class ShopServiceTest {
         ShopService shopService = new ShopService(productRepo);
         //WHEN
         try{
-            Order order2 = new Order(product2.getId(), product2);
+            Order order2 = new Order("2", product2);
             shopService.addOrder(order2);
             fail();
             //THEN
         }catch(IllegalArgumentException e){}
+
+    }
+
+    @Test
+    void addOrder_ShouldContain_AddedOrder(){
+        //GIVEN
+        Product product1 = new Product("1", "iphone 14", 899.9);
+        Product product2 = new Product("2", "iphone 13", 799.9);
+        ProductRepo productRepo = new ProductRepo();
+
+        ShopService shopService = new ShopService(productRepo);
+        shopService.addProduct(product1);
+        shopService.addProduct(product2);
+
+        //WHEN
+        Order order2 = new Order("2", product2);
+        shopService.addOrder(order2);
+
+        //THEN
+        assertThat(shopService.listOrders())
+                .isNotEmpty().containsKey(order2.getId())
+                .doesNotContainKey("1");
+
+
 
     }
 
